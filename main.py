3b5d7911e06691f6fe955de3f89efcb22e98d173
bongstong <nathan.vidal2197@gmail.com>
2025-06-12 17:59:32 +0200

    moved main.py for easier use

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
-from background_handler import BackgroundHandler
-from music_player import MusicPlayer
-from song_handler import SongHandler
+from source.background_handler import BackgroundHandler
+from source.music_player import MusicPlayer
+from source.song_handler import SongHandler
 from os import remove
+from json import load
 
+with open(file="intel.json", mode="r") as file:
+    intel: list = load(file)
 
-path: str = "/run/media/nathanv/ssd1/Music/"
+path: str = ""
+for information in intel:
+    os: str = information.get("OS")
+    path: str = information.get("song_path")
+    cover_path: str = information.get("cover_path")
+
 yeezy: MusicPlayer = MusicPlayer(music_path=path)
 songHandler: SongHandler = SongHandler()
 
 
 def main(path: str = path):
     played_songs: list = songHandler.load_song_list()
-    if len(played_songs) == songHandler.num_songs(path=path):
+    if len(played_songs) == songHandler.num_songs(path=path) is True:
         print("played all songs")
         remove("played_songs.json")
     song: str = yeezy.get_random_song(
@@ -21,7 +29,7 @@ def main(path: str = path):
         current_track=song,
     )
     songInspector.change_background(
-        cover_path="/home/nathanv/Pictures/album_covers/",
+        cover_path=cover_path,
     )
     songHandler.dump_song_list(
         yeezy.handle_played_music(song, played_songs),
