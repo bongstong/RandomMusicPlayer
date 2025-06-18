96ad0fc616443fdee0439b26447d1716d3b0f794
bongstong <nathan.vidal2197@gmail.com>
2025-06-18 13:13:48 +0200

    added song info into the GUI

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from tkinter import BOTH, BOTTOM, Frame, Button, LEFT, TOP, END
 from source.data_handler import DataHandler
 import tkinter as tk
 from source.music_player import MusicPlayer
@@ -13,6 +14,7 @@ songs: list = list()
 operating_system: int = 0
 for information in intel:
     operating_system: int = information.get("OS")
+    de: int = information.get("de")
     path: str = information.get("song_path")
     cover_path: str = information.get("cover_path")
 
@@ -23,50 +25,55 @@ list_of_songs: list = songHandler.all_songs(path=path)
 num_songs: int = songHandler.num_songs(path=path)
 mixData: DataHandler = DataHandler("")
 mixtape_info: list = mixData.mixtape_data(path=path)
-__import__("pprint").pprint(f"mixtape_info: {mixtape_info}")
+gui_data: list = [["ID", "ARTIST", "ALBUM", "TITLE"]]
+index: int = 0
+filenames: list = list()
+for filename in mixtape_info:
+    filenames.append(filename[-1])
+    del filename[-1]
+    filename.insert(0, index)
+    gui_data.append(filename)
+    index += 1
 
 
-def main() -> None:
+def main(random: bool = True, song_to_play: str = "") -> None:
     """main program which handles music playing and background
     changing and data dumping, basically everyting"""
     # music playing system
     played_songs: list = songHandler.load_song_list()
-    print(len(played_songs))
-    print(num_songs)
-    print(len(played_songs) >= num_songs)
     if len(played_songs) >= num_songs:
-        print("yes")
         print("played all songs")
         os.remove("played_songs.json")
-    song: str = yeezy.get_random_song(
-        played_songs=played_songs,
-    )
+    if song_to_play == "":
+        song: str = yeezy.get_random_song(
+            played_songs=played_songs,
+        )
+    else:
+        song: str = song_to_play
     # background changing function/class
     songInspector: DataHandler = DataHandler(
         current_track=song,
     )
     songInspector.change_background(
-        cover_path=cover_path, operating_sys=operating_system
+        cover_path=cover_path, operating_sys=operating_system, de=de
     )
     songHandler.dump_song_list(
         yeezy.handle_played_music(song, played_songs),
     )
-    yeezy.play_random_song()
+    if random is True:
+        yeezy.play_random_song()
 
     return None
 
 
-def get_track_id() -> int:
+def play_song() -> None:
     """func that gets activated when clicking button.
-    gets index of specific song to play it
-    returns the index"""
-    print(int(track_id.get()))
-    return int(track_id.get())
-
-
-def button(text: str, command: str) -> None:
-    button: tk.Button = tk.Button(text=text, command=command)
-    button.pack
+    gets index of specific song and plays it
+    """
+    main(
+        random=False,
+        song_to_play=yeezy.play_specific_song(filenames[int(track_id.get())]),
+    )
     return None
 
 
@@ -74,10 +81,10 @@ while True:
     try:
 
         # window and text input/output
-        window: tk.Tk = tk.Tk()
-        window.title("Jam Player")
+        root: tk.Tk = tk.Tk()
+        root.title("Jam Player")
         label: tk.Label = tk.Label(
-            window,
+            root,
             text="Input track id to play specific song",
         )
         label.pack()
@@ -85,23 +92,44 @@ while True:
         track_id.pack()
 
         # buttons
-        play_button: tk.Button = tk.Button(text="Ok", command=get_track_id)
-        play_button.pack()
-        # quit_button: tk.Button = tk.Button(text="Quit", command=quit)
-        # quit_button.pack()
-        button("Quit", "quit")
-        pause_btn: tk.Button = tk.Button(
-            text="Pause/Unpause",
-            command=yeezy.pause_song,
+        quit_button: tk.Button = tk.Button(text="Quit program", command=quit)
+        play_specific_song: tk.Button = tk.Button(
+            text="Ok",
+            command=play_song,
         )
-        pause_btn.pack()
-        play_random_button: tk.Button = tk.Button(
+        play_specific_song.pack()
+        quit_button.pack()
+
+        top: Frame = Frame(root)
+        bottom: Frame = Frame(root)
+        top.pack(side=TOP)
+        bottom.pack(side=BOTTOM, fill=BOTH, expand=True)
+
+        play_random_button = Button(
+            root,
             text="Play/Skip random song",
+            height=2,
             command=main,
         )
-        play_random_button.pack()
+        pause_btn = Button(
+            root,
+            text="Pause/Unpause",
+            height=2,
+            command=yeezy.pause_song,
+        )
+        play_random_button.pack(in_=top, side=LEFT)
+        pause_btn.pack(in_=top, side=LEFT)
 
-        window.mainloop()
+        grid_frame: Frame = Frame(root)
+        grid_frame.pack(padx=10, pady=10)
+
+        for i in range(len(mixtape_info)):
+            for j in range(len(gui_data[0])):
+                entry: tk.Entry = tk.Entry(grid_frame, width=20)
+                entry.grid(row=i, column=j)
+                entry.insert(END, gui_data[i][j])
+
+        root.mainloop()
 
     except KeyboardInterrupt:
         print("\nAborting program")
