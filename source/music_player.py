7605721b0b554e18b51c72ed5d9a2ac17f98a724
bongstong <nathan.vidal2197@gmail.com>
2025-07-28 09:12:24 +0200

    Optimised GUI and started adding automatic album cover feature

--- source/music_player.py
+++ source/music_player.py
@@ -1,31 +1,66 @@
-# from subprocess import run
-from time import sleep
-from pygame import mixer
+"""program that plays the song, and pseudo-randomly chhooses the song.
+Also includes all related features as pausing the song."""
+
+import pygame
+from os import remove
 from random import seed, randint, shuffle
 from datetime import datetime
 from pathlib import Path
 from os import walk, urandom
-from background_handler import BackgroundHandler
+
+
+class MusicAlreadyPlaying(Exception):
+    """Error handling class"""
+
+    def __init__(self):
+        print("music is already paused")
+        pygame.mixer.music.unpause()
+        return None
 
 
 class MusicPlayer:
+    """handles application, playing music, fetching random songs
+    kwargs: music_pathL str is the path of the mixtape"""
+
     def __init__(self, music_path: str = "~/Music/") -> None:
+        pygame.init()
+        pygame.mixer.init()
         self.path: str = music_path
         self.current_song: str = ""
         return None
 
-    def play(self) -> str:
-        song_info: BackgroundHandler = BackgroundHandler(self.current_song)
-        music_length = song_info.duration
-        print("playing music")
-        mixer.init()
-        mixer.music.load(self.current_song)
-        mixer.music.play()
-        sleep(float(music_length))
-        # run(["play", self.current_song])
+    def play_random_song(self) -> str:
+        """func that plays the song. outputs ths song"""
+        print(f"playing: {self.current_song}")
+        pygame.mixer.music.load(self.current_song)
+        pygame.mixer.music.play()
         return self.current_song
 
+    def play_specific_song(self, song: str) -> str:
+        pygame.mixer.music.load(song)
+        pygame.mixer.music.play()
+        pygame.mixer.music.set_endevent(pygame.USEREVENT + 1)
+        return song
+
+    def pause_song(self) -> None:
+        """func that pauses and unpauses the song if it's playing or not"""
+        print("pausing/starting")
+        global pause
+        try:
+            if pygame.mixer.music.get_busy() is True:
+                pause = True
+                print("music is playing")
+                pygame.mixer.music.pause()
+            else:
+                raise MusicAlreadyPlaying
+        except MusicAlreadyPlaying:
+            pass
+        return None
+
     def get_random_song(self, played_songs: list) -> str:
+        """func that get's random song from playlist
+        kwargs: played_songs: list is the list of already played songs
+        output: randomly selected song"""
         print("getting random song")
         songs: list = list()
         dir: str = str()
@@ -34,17 +69,26 @@ class MusicPlayer:
             pass
         for song in songs:
             song: str = dir + str(Path(song))
-        while self.current_song in played_songs:
-            shuffle(songs)
-            seed(self.get_seed())
-            index: int = randint(0, len(songs))
-            self.current_song: str = dir + "/" + songs[index]
+            while self.current_song in played_songs:
+                shuffle(songs)
+                seed(self.get_seed())
+                index: int = randint(0, len(songs))
+                try:
+                    self.current_song: str = dir + "/" + songs[index]
+                except IndexError:
+                    print(
+                        "###" * 4,
+                        f"\nindex: {index}\nlen(list): {len(songs)}",
+                    )
+                    remove("played_songs.json")
+
         return self.current_song
 
     def get_seed(self) -> int:
-        print("getting seed")
+        """func that get's seed to select random song.
+        ouput: int the seed."""
         seed1: int = int(datetime.now().strftime("%f"))
-        RAND_SIZE = 4
+        RAND_SIZE: int = 4
         random_data = urandom(RAND_SIZE)
         random_seed: int = int.from_bytes(random_data, byteorder="big")
         return random_seed * seed1
@@ -54,6 +98,11 @@ class MusicPlayer:
         played_song: str,
         list_of_played_songs: list,
     ) -> list:
+        """func that adds current playing song to list of already
+        played songs
+        kwargs: played_song: str is the currently played song
+        list_of_played_songs: list is the list of songs that already played
+        output: list of already played songs including the current one"""
         print("adding song to played songs")
         list_of_played_songs.append(played_song)
         return list_of_played_songs
--- source/song_handler.py
+++ source/song_handler.py
@@ -1,12 +1,17 @@
+"""program that does all the filesystem work"""
 from json import load, dump
 from os import walk
 
 
 class SongHandler:
+    """class that handles songs in files, dump and load..."""
     def __init__(self) -> None:
         self.played_songs: list = list()
 
     def load_song_list(self, filename: str = "played_songs.json") -> list:
+        """func that loads songs from JSON file
+        kwargs: filename: str is the name of the file to load from
+        output: list, content of the file"""
         print("loading info from file")
         output: list = list()
         while output == []:
@@ -23,12 +28,26 @@ class SongHandler:
         played_songs: list,
         filename: str = "played_songs.json",
     ) -> str:
+        """func that dumps songs to JSON file
+        kwargs: filename: str is the name of the file to dump to
+        output: last played song"""
         print("dumping from file")
         with open(file=filename, mode="w") as file:
             dump(played_songs, file, indent=4)
         return played_songs[-1]
 
     def num_songs(self, path: str) -> int:
+        """function that returns total number of songs in mixtape
+        kwargs: path: str is the path of the mixtape
+        output: int number of songs"""
         for _, _, songs in walk(path):
             return len(songs)
         return 0
+
+    def all_songs(self, path: str) -> list:
+        """function that returns total songs in mixtape
+        kwargs: path: str is the path of the mixtape
+        output: list list songs"""
+        for _, _, songs in walk(path):
+            return songs
+        return []
